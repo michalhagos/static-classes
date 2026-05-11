50835d21a2b0693e7124cfdde3be367662d985c4
michalhagos <mhayele3@gmail.com>
2026-05-10 23:35:58 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class NameFormatter {
+    // the private constructor that will not allow me to create a new instance of a class
+    private NameFormatter(){}
+
+    // this is the first format method that only deals with the first and last name
+     public static String format;
+
+
+
+
+}
