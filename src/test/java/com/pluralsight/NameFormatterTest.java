1cd33e3ec7f7c6d4a1730715fd6ea458cb75bbff
michalhagos <mhayele3@gmail.com>
2026-05-11 11:24:58 -0500

    Added test to verify simple format returns lastName first

--- src/test/java/com/pluralsight/NameFormatterTest.java
+++ src/test/java/com/pluralsight/NameFormatterTest.java
@@ -1,8 +1,20 @@
 package com.pluralsight;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NameFormatterTest {
+
+    @Test
+    public void format_should_returnLastNameFirst_whenGivenFirstAndLastName() {
+        // arrange
+        String expected = "Bennett, Marcus";
+        // act
+        String result = NameFormatter.format("Marcus", "Bennett");
+        // assert
+        assertEquals(expected, result);
+    }
 
 
 
