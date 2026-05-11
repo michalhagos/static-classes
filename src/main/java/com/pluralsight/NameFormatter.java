49bf4b7853976332095d097e2a05b1fc2de8f08c
michalhagos <mhayele3@gmail.com>
2026-05-11 00:06:51 -0500

    used append to add the prefix to result only if prefix is not empty

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -2,7 +2,8 @@ package com.pluralsight;
 
 public class NameFormatter {
     // the private constructor that will not allow me to create a new instance of a class
-    private NameFormatter(){}
+    private NameFormatter() {
+    }
 
     // this is the first format method that only deals with the first and last name
     public static String format(String firstName, String lastName) {
@@ -19,11 +20,18 @@ public class NameFormatter {
         // lastName is always required so we always append it first added a comma for the format
         result.append(lastName).append(", ");
 
+
+// only add prefix if one was provided for example "Dr." gets added before the first name
+// example: "Dr." gets added before the first name
+        if (!prefix.isEmpty()) {
+            result.append(prefix).append(" ");
+        }
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
 }
